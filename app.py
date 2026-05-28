fa70c4a3085a77bce25ddd2fb1ac619855e29461
raushansharma7061548740-source <raushansharma7061548740@gmail.com>
2026-05-28 18:57:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 # Load Model and Files
 # =========================
 
-model = load_model("lstm_model.h5")
+model = load_model("lstm_model.h5", compile=False)
 
 with open("tokenizer.pkl", "rb") as f:
     tokenizer = pickle.load(f)
