2df49422732cce73fc3df5b69fd0965ff888439f
raushansharma7061548740-source <raushansharma7061548740@gmail.com>
2026-05-28 18:45:44 +0530

    Add footer to Streamlit app
    Added footer to the Streamlit app indicating it was built with Streamlit and TensorFlow.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pickle
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 # =========================
